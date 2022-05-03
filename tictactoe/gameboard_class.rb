f06469e0941808e384188204aa7f980b7712c722
Ganz <loganbellinghamlb@gmail.com>
2022-05-04 00:07:40 +1200

    Privated methods that alter game board

--- tictactoe/gameboard_class.rb
+++ tictactoe/gameboard_class.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'game_text_module'
+require_relative 'string_class'
 
 # the board in which the game is displayed on
 class Gameboard
@@ -12,13 +13,17 @@ class Gameboard
     (1..9).each { |num| tiles_hash["tile#{num}"] = num.to_s }
   end
 
-  def create_board
+  def display_board(player_instance = nil)
+    update_board(player_instance) unless player_instance.nil?
+
     row1 = " #{tiles_hash['tile1']} | #{tiles_hash['tile2']} | #{tiles_hash['tile3']} "
     row2 = " #{tiles_hash['tile4']} | #{tiles_hash['tile5']} | #{tiles_hash['tile6']} "
     row3 = " #{tiles_hash['tile7']} | #{tiles_hash['tile8']} | #{tiles_hash['tile9']} "
     row_separator = '---+---+---'
     puts "#{row1}\n#{row_separator}\n#{row2}\n#{row_separator}\n#{row3}"
   end
+
+  private
 
   def update_board(player_instance)
     player_selected_tile = player_instance.choose_tile
@@ -30,7 +35,6 @@ class Gameboard
     end
 
     # updates selected tile
-    @tiles_hash["tile#{player_selected_tile}"] = player_instance.gameboard_character
-    create_board
+    @tiles_hash["tile#{player_selected_tile}"] = player_instance.gameboard_character.colour
   end
 end
