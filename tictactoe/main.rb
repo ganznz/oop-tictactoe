be3e2af6641182a5d0885919b33081ce373a4a9a
Ganz <loganbellinghamlb@gmail.com>
2022-05-07 20:34:32 +1200

    Update

--- tictactoe/main.rb
+++ tictactoe/main.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
+require_relative 'game_class'
 require_relative 'gameboard_class'
 require_relative 'player_class'
-require_relative 'game_text_module'
 
-player1 = Player.new('ganz', '!')
+game = Game.new
+puts game.welcome_text
+
+plr1_name = game.define_player_name(1)
+plr1_gameboard_char = game.define_player_gameboard_char(plr1_name)
+player1 = Player.new(plr1_name, plr1_gameboard_char)
+
+# plr2_name = game.define_player_name(2)
+
 game_board = Gameboard.new
-
 game_board.display_board
 game_board.display_board(player1)
