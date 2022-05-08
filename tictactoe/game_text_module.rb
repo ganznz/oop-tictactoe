8680035dfb730d3d6114ada29194ea624beec31a
Ganz <loganbellinghamlb@gmail.com>
2022-05-08 21:43:48 +1200

    Added game text functions

--- tictactoe/game_text_module.rb
+++ tictactoe/game_text_module.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'string_class'
+
 # module including text methods that gets printed in-game
 module GameText
   def welcome_text
@@ -7,42 +9,38 @@ module GameText
   end
 
   def player_name_selection_text(plr_no)
-    "What is player ##{plr_no}'s name?"
+    "What is player ##{plr_no}'s name?".green
   end
 
   def player_marker_selection_text(plr_name)
-    "#{plr_name}, what 1-character letter/symbol do you want your gameboard marker to be?"
+    "#{plr_name}, what 1-character letter/symbol do you want your gameboard marker to be?".green
   end
 
   def player_marker_not_one_char_text
-    "\s\s >>Please choose a game marker thats one character in length."
+    "\s\s >>Please choose a game marker thats one character in length.".red
   end
 
   def player_name_first_char_not_alpha_text
-    "\s\s >>Name has to start with a letter."
+    "\s\s >>Name has to start with a letter.".red
   end
 
-  def player_name_too_short_text
-    "\s\s >>Please input a name more than 2 characters in length."
-  end
-
-  def player_name_too_long_text
-    "\s\s >>Please input a name less than 10 characters."
+  def player_name_invalid_length
+    "\s\s >>Please input a name between 3 and 15 characters".red
   end
 
   def player_name_taken_text
-    "\s\s >>That name is already taken!"
+    "\s\s >>That name is already taken!".red
   end
 
   def player_marker_taken_text
-    "\s\s >>That marker is already taken!"
+    "\s\s >>That marker is already taken!".red
   end
 
   def invalid_input_text
-    "\s\s >>Invalid input: Choose a tile between 1-9."
+    "\s\s >>Invalid input: Choose a tile between 1-9.".red
   end
 
   def tile_already_selected_text
-    "\s\s >>This tile is taken, select another one."
+    "\s\s >>This tile is taken, select another one.".red
   end
 end
