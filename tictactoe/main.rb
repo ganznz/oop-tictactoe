1460f83e20f8299be9a2fa674ef2e8d52162dfda
Ganz <loganbellinghamlb@gmail.com>
2022-05-08 21:44:21 +1200

    Changes

--- tictactoe/main.rb
+++ tictactoe/main.rb
@@ -11,7 +11,9 @@ plr1_name = game.define_player_name(1)
 plr1_gameboard_char = game.define_player_gameboard_char(plr1_name)
 player1 = Player.new(plr1_name, plr1_gameboard_char)
 
-# plr2_name = game.define_player_name(2)
+plr2_name = game.define_player_name(2, player1)
+plr2_gameboard_char = game.define_player_gameboard_char(plr2_name, player1)
+player2 = Player.new(plr2_name, plr2_gameboard_char)
 
 game_board = Gameboard.new
 game_board.display_board
