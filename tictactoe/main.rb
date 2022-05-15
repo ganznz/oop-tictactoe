c9069f5bbdc63b773465b18843e7e3878554e875
Ganz <loganbellinghamlb@gmail.com>
2022-05-16 00:47:50 +1200

    Defined unfinalised game loop. Primarily for testing

--- tictactoe/main.rb
+++ tictactoe/main.rb
@@ -4,6 +4,9 @@ require_relative 'game_class'
 require_relative 'gameboard_class'
 require_relative 'player_class'
 
+
+
+# game initialisation
 game = Game.new
 puts game.welcome_text
 
@@ -15,5 +18,9 @@ plr2_name = game.define_player_name(2, player1)
 plr2_gameboard_char = game.define_player_gameboard_char(plr2_name, player1)
 player2 = Player.new(plr2_name, plr2_gameboard_char)
 
-game_board = Gameboard.new
-game_board.display_board
+gameboard = Gameboard.new
+gameboard.display_board_placeholder_text
+
+while !game.end_game?(gameboard)
+  gameboard.display_board(gameboard.determine_next_player)
+end
