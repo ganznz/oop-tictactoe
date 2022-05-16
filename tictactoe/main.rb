53fd2e348bdedc95cb08d2f0037b33ccace49848
Ganz <loganbellinghamlb@gmail.com>
2022-05-16 19:53:43 +1200

    Finalised game loop

--- tictactoe/main.rb
+++ tictactoe/main.rb
@@ -4,23 +4,30 @@ require_relative 'game_class'
 require_relative 'gameboard_class'
 require_relative 'player_class'
 
+run_game = true
 
+while run_game
+  system 'clear' # clears console whenever a new game starts
 
-# game initialisation
-game = Game.new
-puts game.welcome_text
+  # game instance initialisation
+  game = Game.new
+  puts game.welcome_text
 
-plr1_name = game.define_player_name(1)
-plr1_gameboard_char = game.define_player_gameboard_char(plr1_name)
-player1 = Player.new(plr1_name, plr1_gameboard_char)
+  # player1 instance initialisation
+  plr1_name = game.define_player_name(1)
+  plr1_gameboard_char = game.define_player_gameboard_char(plr1_name)
+  player1 = Player.new(plr1_name, plr1_gameboard_char)
 
-plr2_name = game.define_player_name(2, player1)
-plr2_gameboard_char = game.define_player_gameboard_char(plr2_name, player1)
-player2 = Player.new(plr2_name, plr2_gameboard_char)
+  # player2 instance initialisation
+  plr2_name = game.define_player_name(2, player1)
+  plr2_gameboard_char = game.define_player_gameboard_char(plr2_name, player1)
+  player2 = Player.new(plr2_name, plr2_gameboard_char)
 
-gameboard = Gameboard.new
-gameboard.display_board_placeholder_text
+  # gameboard instance initialisation
+  gameboard = Gameboard.new
+  gameboard.display_board_placeholder_text
 
-while !game.end_game?(gameboard)
-  gameboard.display_board(gameboard.determine_next_player)
+  gameboard.display_board(gameboard.determine_next_player) until game.end_game?(gameboard)
+
+  run_game = false unless game.play_another_game?
 end
